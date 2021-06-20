296511aba9215277fb59716c5dee13e908e7881e
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-06-20 15:52:53 +0700

    refactor index with component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
-import Head from 'next/head'
-import Image from 'next/image'
-// import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Navbar from '../components/Navbar';
+import Header from '../components/Header';
 
 export default function Home() {
+  const width = 500;
+  const height = 500;
+
   return (
-    <div>
-      <h1 className='text-red-500'>MUHAMMAD ZHUHRY</h1>
-    </div>
+    <>
+      <Head>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <title>Muhammad Zhuhry</title>
+      </Head>
+      <Navbar />
+      <Header width={width} height={height} />
+    </>
   )
 }
