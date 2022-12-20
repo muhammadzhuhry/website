e9b8158b9934b83e3e40678db0481650e3d8760b
muhammadzhuhry <muhammadzhuhry@gmail.com>
2022-12-20 11:57:57 +0700

    change nextjs images setting

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ const withImages = require('next-images');
 module.exports = withImages({
   reactStrictMode: true,
   images: {
-    disableStaticImages: true
+    disableStaticImages: false
   }
 });
